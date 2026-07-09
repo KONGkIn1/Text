0b7493c3112c61534992ff60485cb73837617c74
Kong <2173508828@qq.com>
2026-07-09 12:40:27 +0800

    启用禁用员工账号业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -91,7 +91,6 @@ public class EmployeeController {
     }
 
     /**
-     *
      * 员工分页查询
      * @param employeePageQueryDTO
      * @return
@@ -102,5 +101,20 @@ public class EmployeeController {
         log.info("员工分页查询，参数为：{}",employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     *  启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation(value = "员工状态启用禁用")
+    public Result startOrStop(@PathVariable Integer status ,Long id){
+        log.info("启用禁用员工账号：{}，{}",status, id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+
     }
 }
