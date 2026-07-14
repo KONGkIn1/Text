54033512dfe8228f05ed1c955da56cac67d4f087
Kong <2173508828@qq.com>
2026-07-14 18:46:08 +0800

    修改菜品

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -5,6 +5,7 @@ import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -20,9 +21,23 @@ public interface DishFlavorMapper {
 
 
     /**
+     * 根据菜品id集合批量删除口味数据
+     * @param dishIds
+     */
+    void deleteByDishIds(@Param("dishIds") List<Long> dishIds);
+
+    /**
      * 根据菜品id删除对应的口味数据
      * @param dishId
      */
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
-    void deleteByDishIds(Long dishId);
+    void deleteByDishId(Long dishId);
+
+    /**
+     * 根据菜品id查询对应的口味数据
+     * @param dishId
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
