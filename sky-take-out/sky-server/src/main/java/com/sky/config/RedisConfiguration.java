05f0ead1b90944e2e9a3bd52abe92a2aa85beb51
Kong <2173508828@qq.com>
2026-07-20 15:26:08 +0800

    修复Redis缓存逻辑及泛型警告

--- sky-take-out/sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-take-out/sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -15,10 +15,8 @@ public class RedisConfiguration {
     @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始创建Redis模版对象...");
-        RedisTemplate redisTemplate = new RedisTemplate();
-        //设置Redis的连接工厂对象
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        //设置Redis key 的序列化器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
