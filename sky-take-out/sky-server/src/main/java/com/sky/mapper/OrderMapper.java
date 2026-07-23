0490d79af399ae992f0e824bc998d54f08b11731
Kong <2173508828@qq.com>
2026-07-23 20:41:42 +0800

    订单状态定时处理

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -7,6 +7,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
 
@@ -53,4 +56,13 @@ public interface OrderMapper {
      */
     @Select("select * from orders where number = #{number}")
     Orders getByNumber(String number);
+
+    /**
+     * 根据订单状态和订单时间查询订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
